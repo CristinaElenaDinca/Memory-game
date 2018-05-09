55029cbcf55a8f845a50014fb12c3b2ee9e8ce47
CristinaElenaDinca <37436448+CristinaElenaDinca@users.noreply.github.com>
2018-05-09 15:31:07 +0300

    Timmer, clicks and stars

--- js/Myscript.js
+++ js/Myscript.js
@@ -1,8 +1,29 @@
 $(".main-board").hide();
 
-let clicks=0;
+let clicks = 0;
+let timer = 0;
 
 $(".start-game").click(function () {
+	let clicks = 0;
+    let timer = 0;
+
+	setInterval(function(){
+		timer += 1;
+		document.querySelector(".timer").innerHTML ="You have started the game " + timer + " seconds ago";
+
+	} ,1000);
+
+	switch(timer) {
+		case 32:
+			$(".star1").hide();
+		case 48:
+			$(".star2").hide();
+		case 64:
+			$(".star3").hide();
+		case 80:
+			$(".star4").hide();
+	};
+
 	let arrayOfOrder = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 	for (let i = 0; i < 200; i++) {
 		let a = Math.floor(Math.random()*16);
@@ -16,10 +37,12 @@ $(".start-game").click(function () {
 		arrayOfDiv[i].style.order = arrayOfOrder[i];
 	};
 	$(".main-board").show();
+
 });
 
 $(".cell").click(function( event ) {
-	clicks=clicks+1;
+	clicks += 1;
+	document.querySelector(".clicks").innerHTML ="You have cliked " + clicks + " times";
 	let card = $(event.target).children().first();
 	card.removeClass("close");
 	card.addClass("open");
@@ -47,3 +70,4 @@ $(".again").click(function() {
 	$(".main-board").show();
 	$(".front").addClass("close").removeClass("solved");
 });
+
